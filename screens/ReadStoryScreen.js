c7ef342f15b06f19186ec239696a260b7cf6c1b8
codingiscool121 <65823748+codingiscool121@users.noreply.github.com>
2020-10-31 17:19:53 -0400

    Add files via upload

---
+++ screens/ReadStoryScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TextInput } from 'react-native';
+
+export default class ReadStoryScreen extends React.Component {
+	render() {
+		return (
+			<View>
+				<Text> Read A Story</Text>
+			</View>
+		);
+	}
+}
